de7389bf10afe9f6ac177e7fe994be8a233bd05e
kimberlybone <k_bone@uncg.edu>
2021-10-18 10:24:16 -0400

    can add todo item to list

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .models import TodoItem
 # Create your views here.
 
 def todoView(request):
-    return render(request, 'todo.html')
+    todo_items = TodoItem.objects.all()
+    
+    return render(request, 'todo.html', 
+        {'all_items': todo_items})
+
+# create new todo and save it
+# redirect browser to /todo/
+def addTodo(request):
+    new_item = TodoItem(content = request.POST['content'])
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
